3d511d88984c1c83f1a4795c3783604d8fadd446
root <adamamuhammad01@gmail.com>
2023-08-12 03:31:18 -0700

    updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,25 @@
 #!/usr/bin/node
-
-// a script that prints all characters of a Star Wars movie
-
 const request = require('request');
-const movies = process.argv.slice(2);
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-request.get(`https://swapi-api.hbtn.io/api/films/${movies}/`, async (_error, response, body) => {
-  for (const character of JSON.parse(body).characters) {
-    const people = await new Promise((resolve, reject) => {
-      request(character, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(JSON.parse(body).name);
-        }
-      });
-    });
-    console.log(people);
-  }
-});
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
